0e8bab44fd7dfef6f986e00c016415aa1a20f3f7
Eduart2021 <zeedymarina@yahoo.com>
2022-08-13 23:24:54 -0400

    first

---
+++ test/new.spec.js
@@ -0,0 +1,33 @@
+const { remote } = require("webdriverio")
+
+
+describe("New In", () => {
+  it("Sample", async () => {
+    const browser = await remote({
+      capabilities: {
+        browserName: "chrome",
+      },
+    });
+
+    await browser.url("https://www.zedy3d.com/blank-pageab3a8782");
+
+    const div = await browser.waitUntil(async () => {
+      const apiLink = await browser.$$('div[class="photogallery-column  column-4"]')
+      console.log(apiLink.length);
+      if (apiLink <=1) {
+          return false
+      }
+      return apiLink[19]
+  }, {
+      timeoutMsg: 'Never found elements'
+  })
+  await div.click()
+
+    // const apiLink = await browser.$('li[id="1848609637"]')
+    // //li[id="1848609637"]
+    // await apiLink.click();
+
+    await browser.saveScreenshot("./screenshot.png");
+    await browser.deleteSession();
+  });
+});
