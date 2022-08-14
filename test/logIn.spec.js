a25d92c41545b5f78b6c2562240242bb954864c2
Eduart2021 <zeedymarina@yahoo.com>
2022-08-14 16:11:37 -0400

    US-2

--- test/logIn.spec.js
+++ test/logIn.spec.js
@@ -1,7 +1,12 @@
-
+import LoginPage from  '../../login.page';
+import SecurePage from '../../secure.page';
 
 describe('login credentials',()=>{
     it('should login',()=>{
+        LoginPage.open()
+       
+        LoginPage.login('tomsmith','SuperSecretPassword!')
 
+        expect(SecurePage.flashAlert).toHaveTextContaining('hello')
     })
 })
