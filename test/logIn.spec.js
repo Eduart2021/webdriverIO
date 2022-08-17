90d8b3e412ca11d4fd0aed79ca83f188dbbb4ea0
Eduart2021 <zeedymarina@yahoo.com>
2022-08-16 22:37:33 -0400

    add new test

--- test/logIn.spec.js
+++ test/logIn.spec.js
@@ -9,6 +9,15 @@ describe('login credentials',()=>{
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!')
-
+        await LoginPage.logOut()
+    })
+})
+describe('Log out',()=>{
+    it("Should Log out", async ()=>{
+        await LoginPage.open();
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await LoginPage.logOut()
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            "You logged out of the secure area!")
     })
 })
